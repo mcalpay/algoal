f2a8afde451ff2c451f868a972cca28e026ff9de
mcalpay <muratcan.alpay@t2.com.tr>
2015-07-20 10:34:51 +0300

    sorting

--- src/main/java/org/mca/algoal/sorting/InsertionSorter.java
+++ src/main/java/org/mca/algoal/sorting/InsertionSorter.java
@@ -2,6 +2,7 @@ package org.mca.algoal.sorting;
 
 import java.util.Comparator;
 
+// gnome sort?
 public class InsertionSorter<T> implements Sorter<T> {
 
     private Comparator<T> comparator;
@@ -13,12 +14,8 @@ public class InsertionSorter<T> implements Sorter<T> {
     @Override
     public void sort(T[] items) {
         for (int i = 1; i < items.length; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (comparator.compare(items[j + 1], items[j]) < 0) {
-                    SorterUtils.swap(items, j + 1, j);
-                } else {
-                    break;
-                }
+            for (int j = i - 1; j >= 0 && comparator.compare(items[j + 1], items[j]) < 0; j--) {
+                SorterUtils.swap(items, j + 1, j);
             }
         }
     }
