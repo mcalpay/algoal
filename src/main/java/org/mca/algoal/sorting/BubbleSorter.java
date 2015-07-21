08a103070721ed89bec4dc89c19573acc30b9bb9
mcalpay <muratcan.alpay@t2.com.tr>
2015-07-21 17:39:09 +0300

    bubblesort imp

--- src/main/java/org/mca/algoal/sorting/BubbleSorter.java
+++ src/main/java/org/mca/algoal/sorting/BubbleSorter.java
@@ -7,10 +7,16 @@ public class BubbleSorter implements Sorter {
     @Override
     public void sort(Comparable[] items) {
         for (int i = 0; i < items.length; i++) {
+            boolean swapped = false;
             for (int j = (i + 1); j < items.length; j++) {
                 if (items[i].compareTo(items[j]) > 0) {
                     Utils.swap(items, i, j);
+                    swapped = true;
                 }
+            }
+
+            if (!swapped) {
+                break;
             }
         }
     }
--- src/main/java/org/mca/algoal/sorting/BubbleSorterWithStream.java
+++ src/main/java/org/mca/algoal/sorting/BubbleSorterWithStream.java
@@ -2,16 +2,24 @@ package org.mca.algoal.sorting;
 
 import org.mca.algoal.utils.Utils;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.IntStream;
 
 public class BubbleSorterWithStream implements Sorter {
 
     @Override
     public void sort(Comparable[] items) {
-        IntStream.range(0, items.length)
-                .forEach(i -> IntStream.range(i + 1, items.length)
-                        .filter(j -> items[i].compareTo(items[j]) > 0)
-                        .forEach(j -> Utils.swap(items, i, j)));
+        AtomicBoolean swapped = new AtomicBoolean(true);
+        IntStream.range(0, items.length).filter(i -> swapped.get())
+                .forEach(i -> {
+                    swapped.set(false);
+                    IntStream.range(i + 1, items.length)
+                            .filter(j -> items[i].compareTo(items[j]) > 0)
+                            .forEach(j -> {
+                                Utils.swap(items, i, j);
+                                swapped.set(true);
+                            });
+                });
     }
 
 }
